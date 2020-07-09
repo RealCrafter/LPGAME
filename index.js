b96eb053ad836e81fd0880717bac6ea283344099
RealCrafter <67182201+RealCrafter@users.noreply.github.com>
2020-07-09 12:35:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const discord = require('discord.js');
+const prefix = "lp:";
+const client = new discord.Client();
+
+client.on("message", message => {
+  if (message.content === prefix + "help") {
+    message.channel.send({
+      embed: {
+        title: "Command Dashboard für LpGames",
+        description: "",
+        color: "#239B56",
+        fields: [
+          { name: "ip von server", value: "lp:ip" },
+          { name: "Link zun forum", value: "lp:forum"}
+        
+        ]
+      }
+    });
+  }
+});
+
+client.on("message", message => {
+  if (message.content === prefix + "ip") {
+    message.channel.send({
+      embed: {
+        title: "IP und PORT",
+        description: "",
+        color: "#239B56",
+        fields: [
+          { name: "", value: "IP: lpgames.deinmc.de" },
+        
+        ]
+      }
+    });
+  }
+});
+
+client.on("message", message => {
+  if(message.content === prefix + "forum") {
+    message.channel.send("Forum von unseren Server! ")
+  }
+})
+
+
+client.login("NzI1Nzg5OTYxMDUxMTc3MDUx.XvT24g.QcAh3Iq1ee-DXx6kecp4Fs4_VYA");
